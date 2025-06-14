2589d73c1a6625506382176fbfa9229df1aef575
citrasari12345 <citrasariagustina@gmail.com>
2025-06-14 20:52:31 +0700

    Morphology of Basidiomycota

---
+++ story_content/user.js
@@ -0,0 +1,120 @@
+function ExecuteScript(strId)
+{
+  switch (strId)
+  {
+      case "62wR5E9EbLc":
+        Script1();
+        break;
+      case "6KXYyKMDYza":
+        Script2();
+        break;
+      case "6kqNDycZS8B":
+        Script3();
+        break;
+      case "6BfNdV8CKTk":
+        Script4();
+        break;
+      case "6dIRhNXlRXy":
+        Script5();
+        break;
+      case "6fOrN09AXfl":
+        Script6();
+        break;
+      case "6QZB7qNkaYU":
+        Script7();
+        break;
+      case "6EakNqpdQYz":
+        Script8();
+        break;
+      case "67fi2iG9Zze":
+        Script9();
+        break;
+      case "5p23qYQ1OJz":
+        Script10();
+        break;
+      case "63GPKr11xAG":
+        Script11();
+        break;
+      case "6qyQKZ6FnBs":
+        Script12();
+        break;
+  }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSongku');
+audio.src="musikku.mp3";
+
+audio.load();
+
+audio.play();
+audio.volume=0.3;
+}
+
+function Script2()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 0.0;
+}
+
+function Script3()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 0.1;
+}
+
+function Script4()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 0.2;
+}
+
+function Script5()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 0.3;
+}
+
+function Script6()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 0.4;
+}
+
+function Script7()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 0.5;
+}
+
+function Script8()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 0.6;
+}
+
+function Script9()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 0.7;
+}
+
+function Script10()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 0.8;
+}
+
+function Script11()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 0.9;
+}
+
+function Script12()
+{
+  var audio = document.getElementById('bgSongku');
+audio.volume = 1.0;
+}
+
